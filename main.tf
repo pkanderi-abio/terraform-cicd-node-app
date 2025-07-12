1d96fbc8719a457e35a1df066c47a65afb47c491
Prasannakumar Kanderi <mail2kanderi@gmail.com>
2025-07-11 20:13:19 -0500

    Add missing variables for subnet_cidrs and availability_zones

--- main.tf
+++ main.tf
@@ -24,6 +24,18 @@ variable "vpc_cidr" {
   default     = "10.0.0.0/16"
 }
 
+variable "subnet_cidrs" {
+  description = "List of subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["us-west-2a", "us-west-2b", "us-west-2c", "us-west-2d"]
+}
+
 variable "my_ip" {
   description = "Your IP address for SSH access"
   type        = string
